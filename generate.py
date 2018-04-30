88da6f789be2ed9483f7f3e09224202eb852f0ac
bvezilic <banex93@gmail.com>
2018-04-30 21:11:17 +0200

    Working on sampling

--- generate.py
+++ generate.py
@@ -1,9 +1,13 @@
+import os.path as osp
 from torch.distributions import Categorical
 import torch
 
 
-def sample():
-    pass
+def sample(model_name):
+    rnn = torch.load(osp.join('models', model_name))
+
+    with torch.no_grad():
+        pass
 
 
 if __name__ == '__main__':
