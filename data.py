1d27f8f1de6bb0bf397f26be1227c5643d830842
Branislav Vezilic <banex93@gmail.com>
2018-06-10 20:42:01 -0400

    Changing dataset

--- data.py
+++ data.py
@@ -1,8 +1,9 @@
-import numpy as np
-from torch.utils.data import Dataset
-
 import os.path as osp
 import string
+import glob
+import numpy as np
+
+from torch.utils.data import Dataset
 
 
 class DnDCharacterNameDataset(Dataset):
@@ -10,15 +11,16 @@ class DnDCharacterNameDataset(Dataset):
         self.root_dir = root_dir
         self.transform = transform
         self.target_transform = target_transform
-        self.files = ['male.txt', 'female.txt']
         self.train_data = []
         self.target_data = []
 
-        for filename in self.files:
-            with open(osp.join(root_dir, filename), 'r') as f:
-                names = f.read().lower().replace(',', '').split()
-                self.train_data.extend([list(name) for name in names])
-                self.target_data.extend([list(name[1:]) + ["\n"] for name in names])
+        races, genders = Races(), Genders()
+        for filename in glob.glob('data/*.txt'):
+            race, gender = osp.basename(filename).split('_')
+            with open(filename, 'r') as f:
+                names = f.read().replace(',', '').split()
+                self.train_data.extend([list(name) + [races[race]] + [gender[gender]] for name in names])
+                self.target_data.extend([list(name[1:]) + ["."] for name in names])
 
     def __len__(self):
         return len(self.train_data)
@@ -34,6 +36,26 @@ class DnDCharacterNameDataset(Dataset):
             target = self.target_transform(target)
 
         return train, target
+
+
+class Races:
+    def __init__(self):
+        self.available_races = ['human', 'elf', 'dwarf', 'half_orc', 'halfling', 'tiefling', 'dragonborn']
+        self.races = dict(zip(self.available_races, np.arange(len(self.available_races))))
+
+    def __len__(self):
+        return len(self.races)
+
+    def __getitem__(self, item):
+        return self.races.get(item)
+
+
+class Genders:
+    def __init__(self):
+        self.genders = {'male': 0, 'female': 1}
+
+    def __getitem__(self, item):
+        return self.genders.get(item)
 
 
 class Vocabulary:
